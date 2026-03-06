b21de28809971212eca47bb85ebd3a0fc2e42998
Takeru Matsuda <91131031+takeru-matsuda@users.noreply.github.com>
2026-03-06 22:50:12 +0900

    Refactor karuta_HMM_recog function for efficiency

--- karuta_HMM_recog.m
+++ karuta_HMM_recog.m
@@ -6,29 +6,29 @@ function [recog_time,recog_fuda,posterior]=karuta_HMM_recog(mfcc,model,threshold
     N = size(mean_vec_i_m,2);
     T = size(mfcc,2);
     ll = zeros(num_fuda,T);
-    posterior = zeros(num_fuda,T);
     for k=1:num_fuda
         mean_vec_i = mean_vec_i_m(:,:,k);
         var_vec_i = var_vec_i_m(:,:,k);
         a_i_j = a_i_j_m(:,:,k);
         filt = [1; zeros(N-1,1)];
+        tmp = zeros(N-2,1);
         for t=1:T
             l = 0;
             pred = filt'*a_i_j;
             for i=2:N-1 
                 l = l + pred(i)*exp(logDiagGaussian(mfcc(:,t),mean_vec_i(:,i),var_vec_i(:,i)));
-                filt(i) = pred(i)*exp(logDiagGaussian(mfcc(:,t),mean_vec_i(:,i),var_vec_i(:,i)));
+                tmp(i-1) = logDiagGaussian(mfcc(:,t),mean_vec_i(:,i),var_vec_i(:,i));
             end
-            if t==1
-                ll(k,t) = log(l);
-            else
-                ll(k,t) = ll(k,t-1)+log(l);
-            end
+            ll(k,t) = log(l);
+            filt = pred.*[0; exp(tmp-max(tmp)); 0];
             filt = filt/sum(filt);
         end
     end
-    for t=1:T
-        posterior(:,t) = exp(w*(ll(:,t)-max(ll(:,t))));
+    posterior = zeros(num_fuda,T);
+    posterior(:,1) = exp(w*(ll(:,1)-max(ll(:,1))));
+    posterior(:,1) = posterior(:,1)/sum(posterior(:,1));
+    for t=2:T
+        posterior(:,t) = posterior(:,t-1).*exp(w*(ll(:,t)-max(ll(:,t))));
         posterior(:,t) = posterior(:,t)/sum(posterior(:,t));
     end
     recog_time = inf;
